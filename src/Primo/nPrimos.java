442afa1c0ef07d40d34d3d5f62c2f07e59292382
Felix <rojofelix20@gmail.com>
2024-05-22 12:26:51 +0200

    numeros primos

--- src/Primo/nPrimos.java
+++ src/Primo/nPrimos.java
@@ -1,5 +1,30 @@
 package Primo;
 
+import java.util.Scanner;
+
 public class nPrimos {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("ingrese un numero");
+		Scanner teclado = new Scanner(System.in);
+		int num = teclado.nextInt();
+		int i = 0;
+		int j = 0;
+		int cont = 0;
+		while (i < num) {
+			if (num % i == 0) {
+				cont++;
+			}
+			i++;
+		}
+		if (cont == 2) {
+			System.out.println("es primo");
+		}else {
+			System.out.println("no es primo");
+		}
+		
+	}
+		
 
 }
